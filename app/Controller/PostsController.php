0a209e8e4f45ed3923ce837fea80230218cf93bd
Franco Ricchi <francoricchi@gmail.com>
2015-11-10 13:08:56 -0300

    Added user reference to Posts.

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -19,6 +19,7 @@ class PostsController extends AppController {
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Post->create();
+			$this->request->data['Post']['user_id'] = $this->Auth->user('id');
 			if ($this->Post->save($this->request->data)) {
 				$this->Flash->success(__('Your post has been saved.'));
 				if ($this->request->is('ajax')) {
